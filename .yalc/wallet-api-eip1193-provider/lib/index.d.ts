28809a9d39729ecd74cdf27a0efbba0a9a1a768f
Hugo Morosini <hugo.morosini@gmail.com>
2023-02-06 10:38:52 +0100

    remove .yalc

--- .yalc/wallet-api-eip1193-provider/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./WalletApiEip1193Provider";
-//# sourceMappingURL=index.d.ts.map
--- .yalc/wallet-api-eip1193-provider/lib/types.d.ts
+++
@@ -1,6 +0,0 @@
-export type NetworkConfig = {
-    currencyId: string;
-    chainId: number;
-    nodeURL: string;
-};
-//# sourceMappingURL=types.d.ts.map
